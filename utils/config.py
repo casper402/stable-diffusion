e43018e5276c4d2c1ef489d741f2e9be0de96f52
au683424 <202008583@post.au.dk>
2025-03-25 17:48:08 +0100

    config changes

--- utils/config.py
+++ utils/config.py
@@ -12,15 +12,9 @@ def get_device():
 def load_config(device, path="config.yaml"):
     with open(path, "r") as f:
         config = yaml.safe_load(f)
-    
-    base_path = config["device_paths"][device.type]
 
-    paths = {
-        key: f"{base_path}/{subpath}"
-        for key, subpath in config["data_paths"].items()
-    }
-    config["paths"] = paths
-    config["train"]["batch_size"] = config["train"]["batch_sizes"][device.type]
+    if device.type == "cpu":
+        config["train"]["batch_size"] = 1
 
     float_keys = ["learning_rate", "min_learning_rate", "fine_tune_lr", "min_fine_tune_lr", "weight_decay", "fine_tune_weight_decay"]
     for key in float_keys:

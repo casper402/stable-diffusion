3e6266ace92ecfe08d4e44bc28e69bbd226734dd
au683424 <202008583@post.au.dk>
2025-04-09 21:13:56 +0200

    new dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -32,29 +32,52 @@ class CBCTtoCTDataset(Dataset):
         self.CBCT_slices = self._collect_slices(CBCT_path)
         self.CT_slices = self._collect_slices(CT_path)
         self.transform = transform
+        self.paired_slice_paths = self._find_paired_slices()
+
+    def _find_paired_slices(self):
+        paired_paths = []
+        ct_volumes = sorted([
+            d for d in os.listdir(self.CT_path)
+            if os.path.isdir(os.path.join(self.CT_path, d)) and d.startswith('volume-')
+        ])
+
+        for ct_volume_name in ct_volumes:
+            volume_num_str = ct_volume_name.split('-')[-1]
+            cbct_rec_name = f"REC-{volume_num_str}"
+
+            ct_volume_path = os.path.join(self.CT_path, ct_volume_name)
+            # *** Assume corresponding CBCT directory exists ***
+            cbct_rec_path = os.path.join(self.CBCT_path, cbct_rec_name)
+
+            # List and sort slices within the CT volume for deterministic order
+            ct_slices = sorted([
+                f for f in os.listdir(ct_volume_path)
+                if os.path.isfile(os.path.join(ct_volume_path, f)) # Simple file check is okay
+            ])
+
+            for slice_name in ct_slices:
+                ct_slice_path = os.path.join(ct_volume_path, slice_name)
+                # *** Construct corresponding CBCT slice path directly ***
+                # *** Assume this file exists because of the guarantee ***
+                cbct_slice_path = os.path.join(cbct_rec_path, slice_name)
+
+                # Add the pair without checking os.path.isfile(cbct_slice_path)
+                paired_paths.append((cbct_slice_path, ct_slice_path))
+        return paired_paths
         
-    def _collect_slices(self, dataset_path):
-        slice_paths = []
-        for subdir in os.listdir(dataset_path):
-            subdir_path = os.path.join(dataset_path, subdir)
-            if os.path.isdir(subdir_path):
-                for slice_name in os.listdir(subdir_path):
-                    slice_path = os.path.join(subdir_path, slice_name)
-                    slice_paths.append(slice_path)
-        return slice_paths
-    
     def __len__(self):
-        return min(len(self.CBCT_slices), len(self.CT_slices))
+        return len(self.paired_slice_paths)
     
     def __getitem__(self, idx):
-        CBCT_path = self.CBCT_slices[idx]
-        CT_path = self.CT_slices[idx]
+        if idx >= len(self):
+            raise IndexError("Index out of range")
+        cbct_slice_path, ct_slice_path = self.paired_slice_paths[idx]
 
-        CBCT_slice = Image.open(CBCT_path).convert("L")
-        CT_slice = Image.open(CT_path).convert("L")
-        
+        cbct_slice = Image.open(cbct_slice_path).convert('L')
+        ct_slice = Image.open(ct_slice_path).convert('L')
+
         if self.transform:
-            CBCT_slice = self.transform(CBCT_slice)
-            CT_slice = self.transform(CT_slice)
+            cbct_slice = self.transform(cbct_slice)
+            ct_slice = self.transform(ct_slice)
 
-        return CBCT_slice, CT_slice
+        return cbct_slice, ct_slice

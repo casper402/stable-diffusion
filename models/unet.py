2f529995df7cdc05bbe8a3a672dbb0c449626d35
au683424 <202008583@post.au.dk>
2025-04-15 11:40:41 +0200

    conditional unet experiment

--- models/unet.py
+++ models/unet.py
@@ -48,7 +48,7 @@ class TimestepEmbedding(nn.Module):
         self.act = nn.SiLU()
         self.linear2 = nn.Linear(dim * 4, dim)
 
-    def forward(self, timesteps): #TODO: Use time embedding implementation form ldm: https://github.com/CompVis/latent-diffusion/blob/main/ldm/modules/diffusionmodules/model.py#L218
+    def forward(self, timesteps):
         half_dim = self.dim // 2
         emb = math.log(10000.0) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
@@ -107,6 +107,29 @@ class AttentionBlock(nn.Module):
         h_ = h_.transpose(1, 2).view(b, c, h, w)
         return x + h_
     
+class CrossAttentionBlock(nn.Module):
+    def __init__(self, channels, num_heads=8):
+        super().__init__()
+        self.num_heads = num_heads
+        self.norm = Normalize(channels)
+        self.mha = nn.MultiheadAttention(
+            embed_dim=channels,
+            num_heads=num_heads,
+            batch_first=True # Crucial for our reshaping (B, L, E)
+        )
+    def forward(self, x, context):
+        h_ = x
+        h_ = self.norm(h_)
+        b, c, h, w = h_.shape
+        h_ = h_.view(b, c, h * w).transpose(1, 2)
+
+        context = self.norm(context)
+        context = context.view(b, c, h * w).transpose(1, 2)
+
+        h_, _ = self.mha(h_, context, context)
+        h_ = h_.transpose(1, 2).view(b, c, h, w)
+        return x + h_
+    
 class DownBlock(nn.Module):
     def __init__(self, in_channels, out_channels, time_emb_dim=None, has_attn=False, num_heads=8, dropout_rate=0.1):
         super().__init__()
@@ -154,13 +177,13 @@ class MiddleBlock(nn.Module):
     def __init__(self, in_channels, time_emb_dim=None, num_heads=8, dropout=0.1):
         super().__init__()
         self.res_block1 = ResnetBlock(in_channels, in_channels, time_emb_dim, dropout)
-        self.attention1 = AttentionBlock(in_channels, num_heads=num_heads)
+        self.attention1 = CrossAttentionBlock(in_channels, num_heads=num_heads)
         self.res_block2 = ResnetBlock(in_channels, in_channels, time_emb_dim, dropout)
 
-    def forward(self, x, temb=None):
+    def forward(self, x, context, temb=None):
         h = x
         h = self.res_block1(h, temb)
-        h = self.attention1(h)
+        h = self.attention1(h, context)
         h = self.res_block2(h, temb)
         return h
 
@@ -199,15 +222,19 @@ class UNet(nn.Module): # Modified UNet for ControlNet
         self.final_norm = Normalize(ch1) # Norm before final conv
         self.final_conv = nn.Conv2d(ch1, out_channels, kernel_size=3, stride=1, padding=1)
 
-    def forward(self, x, t):
+    def forward(self, x, context, t):
         t_emb = self.time_embedding(t)
         x = self.init_conv(x)         # Initial convolution: [B, 4, H, W] -> [B, 64, H, W]
-
         x, skip1 = self.down1(x, t_emb)   # -> [B, 128, H/2, W/2]
         x, skip2 = self.down2(x, t_emb)  # -> [B, 256, H/4, W/4]
         x, skip3 = self.down3(x, t_emb)  # -> [B, 512, H/8, W/8]
 
-        x = self.middle(x, t_emb) # -> [B, 512, H/8, W/8]
+        context = self.init_conv(context)
+        context, _ = self.down1(context)
+        context, _ = self.down2(context)
+        context, _ = self.down3(context)
+
+        x = self.middle(x, context, t_emb) # -> [B, 512, H/8, W/8]
 
         x = self.up3(x, skip3, t_emb)    # -> [B, 256, H/4, W/4]
         x = self.up2(x, skip2, t_emb)     # -> [B, 128, H/2, W/2]

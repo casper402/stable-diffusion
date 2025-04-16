eae8e243369ef34f1ce3ccc45945a10496837772
Niklas Aavad <niklasaavad@gmail.com>
2025-04-16 21:35:00 +0200

    fix one by off because why not

--- utils/dataset.py
+++ utils/dataset.py
@@ -42,7 +42,7 @@ class CTDatasetNPY(Dataset):
         slice_paths = []
         count = 0
         for slice_name in os.listdir(dataset_path):
-            if limit is not None and count > limit:
+            if limit is not None and count >= limit:
                 break
             if slice_name.endswith('.npy'):
                 slice_paths.append(slice_name)

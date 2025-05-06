93df1f7b5fe082c91318d56d05908b51c4b01b28
Niklas Aavad <niklasaavad@gmail.com>
2025-05-06 10:26:29 +0200

    infer with new model

--- quick_loop/speed_batch_predict.py
+++ quick_loop/speed_batch_predict.py
@@ -16,7 +16,7 @@ from quick_loop.unetControlPACA import load_unet_control_paca
 # ------------------------
 CBCT_DIR = '../training_data/scaled-490/'
 VOLUME_INDICES = [3, 8, 12, 26, 32, 33, 35, 54, 59, 61, 106, 116, 129]
-OUT_DIR = '../predictionsV2-490-stepsize20/'
+OUT_DIR = '../predictionsV2-490-50steps_v2/'
 
 GUIDANCE_SCALE = 1.0
 BATCH_SIZE = 32  # tune as needed
@@ -26,7 +26,7 @@ POWER_P = 2.0       # power-law exponent for smoothing
 FINE_CUTOFF = 9     # switch to single-step updates at t<=9 (last 10 steps)
 STEP_SIZE = 20
 
-MODELS_PATH = 'controlnet_v2_inference/'
+MODELS_PATH = 'controlnet_v2_inference_v2/'
 VAE_SAVE_PATH = os.path.join(MODELS_PATH, 'vae_joint_vae.pth')
 UNET_SAVE_PATH = os.path.join(MODELS_PATH, 'unet_joint_unet.pth')
 PACA_LAYERS_SAVE_PATH = os.path.join(MODELS_PATH, 'paca_layers.pth')

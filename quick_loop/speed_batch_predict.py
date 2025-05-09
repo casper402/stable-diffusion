8814d741894a5d387f6601a648ab7a50d47a6c5e
Niklas Aavad <niklasaavad@gmail.com>
2025-05-09 10:25:02 +0200

    prepare for perceptual evaluation

--- quick_loop/speed_batch_predict.py
+++ quick_loop/speed_batch_predict.py
@@ -14,13 +14,13 @@ from quick_loop.unetControlPACA import load_unet_control_paca
 # ------------------------
 # Configuration Variables
 # ------------------------
-PREDICT_CLINIC = True
+PREDICT_CLINIC = False
 
 CBCT_DIR = '../training_data/scaled-490/'
 CBCT_CLINIC_DIR = '../training_data/clinic/'
 VOLUME_INDICES = [3, 8, 12, 26, 32, 33, 35, 54, 59, 61, 106, 116, 129]
-OUT_DIR = '../prediction-clinic-20stepsize/'
-# OUT_DIR = '../prediction-490-stepsize20-perceptual/'
+# OUT_DIR = '../prediction-clinic-20stepsize/'
+OUT_DIR = '../prediction-490-stepsize20-perceptual/'
 
 GUIDANCE_SCALE = 1.0
 ALPHA_A = 0.2         # Mixing weight for CBCT signal at t0
@@ -31,7 +31,8 @@ POWER_P = 2.0         # power-law exponent for smoothing
 FINE_CUTOFF = 9       # switch to single-step updates at t<=9 (last 10 steps)
 STEP_SIZE = 20
 
-MODELS_PATH = 'controlnet_v2_inference_v2/'
+# MODELS_PATH = 'controlnet_v2_inference_v2/'
+MODELS_PATH = 'controlnet_with_decoding_loss_perceptual_lower '
 VAE_SAVE_PATH = os.path.join(MODELS_PATH, 'vae_joint_vae.pth')
 UNET_SAVE_PATH = os.path.join(MODELS_PATH, 'unet_joint_unet.pth')
 PACA_LAYERS_SAVE_PATH = os.path.join(MODELS_PATH, 'paca_layers.pth')

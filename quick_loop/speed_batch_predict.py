f5b2e4a0842520071ceda48561eb6abef97a450f
Niklas Aavad <niklasaavad@gmail.com>
2025-05-09 10:19:22 +0200

    prepare for predictions of perceptual loss model

--- quick_loop/speed_batch_predict.py
+++ quick_loop/speed_batch_predict.py
@@ -17,7 +17,8 @@ from quick_loop.unetControlPACA import load_unet_control_paca
 CBCT_DIR = '../training_data/scaled-490/'
 CBCT_CLINIC_DIR = '../training_data/clinic/'
 VOLUME_INDICES = [3, 8, 12, 26, 32, 33, 35, 54, 59, 61, 106, 116, 129]
-OUT_DIR = '../prediction-clinic-20stepsize/'
+# OUT_DIR = '../prediction-clinic-20stepsize/'
+OUT_DIR = '../prediction-490-stepsize20-perceptual/'
 
 GUIDANCE_SCALE = 1.0
 ALPHA_A = 0.2         # Mixing weight for CBCT signal at t0
@@ -26,7 +27,7 @@ BATCH_SIZE = 32       # tune as needed
 DDIM_STEPS = 40       # total coarse sampling steps
 POWER_P = 2.0         # power-law exponent for smoothing
 FINE_CUTOFF = 9       # switch to single-step updates at t<=9 (last 10 steps)
-STEP_SIZE = 20
+STEP_SIZE = 1
 
 MODELS_PATH = 'controlnet_v2_inference_v2/'
 VAE_SAVE_PATH = os.path.join(MODELS_PATH, 'vae_joint_vae.pth')
@@ -122,7 +123,7 @@ def predict_volume(
             mu, logvar = vae.encode(imgs)
 
             # Tried with CBCT injected noise, but returned to random noise for now
-            z = torch.randn_like(mu
+            z = torch.randn_like(mu)
 
             # PACA control diffusion loop
             for i in range(len(schedule) - 1):
@@ -192,6 +193,6 @@ def predict_clinic():
 # ------------------------
 
 if __name__ == '__main__':
-    # predict_test_data()
+    predict_test_data()
     
-    predict_clinic()
+    # predict_clinic()

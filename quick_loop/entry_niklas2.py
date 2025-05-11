d8e52a72ad30e1befb8f5a3f4cd2b08a314e7ad0
Niklas Aavad <niklasaavad@gmail.com>
2025-05-11 20:39:47 +0200

    prepare to continue training v3 (now v4)

--- quick_loop/entry_niklas2.py
+++ quick_loop/entry_niklas2.py
@@ -21,7 +21,7 @@ accumulation_steps = 1 # Effectively increases batch size to batch_size * accumu
 num_workers = 8
 epochs = 2000
 early_stopping = 50
-patience = 10
+patience = 15
 epochs_between_prediction = 5
 base_channels = 256
 dropout_rate = 0.1
@@ -30,16 +30,20 @@ warmup_lr = 1e-8
 warmup_epochs = 5
 
 # Load pretrained model paths
-load_dir = "unet_with_decoding_loss_perceptual_lower"
+load_dir = "controlnet_v3"
 load_vae_path = os.path.join(load_dir, "vae_joint.pth")
 load_unet_path = os.path.join(load_dir, "unet_joint.pth")
+load_controlnet_path = os.path.join(load_dir, "controlnet.pth")
+load_paca_layers_path = os.path.join(load_dir, "paca_layers.pth")
+load_degradation_removal_path = os.path.join(load_dir, "dr_module.pth")
 
 # Save prediction / model directories
 # save_dir = "controlnet_v2"
 # save_dir = "unet_with_decoding_loss_perceptual_lower_v2"
 # save_dir = "train_from_joint"
 # save_dir = "controlnet_v3"
-save_dir = "controlnet_from_unet_trained_after_joint"
+# save_dir = "controlnet_from_unet_trained_after_joint"
+save_dir = "controlnet_v4"
 
 os.makedirs(save_dir, exist_ok=True)
 vae_predict_dir = os.path.join(save_dir, "vae_predictions")
@@ -153,11 +157,13 @@ augmentation = {
 #     epochs_between_prediction=epochs_between_prediction,
 # )
 
-vae = load_vae(save_path=vae_save_path, trainable=False)
-unet = load_unet_control_paca(unet_save_path=unet_save_path, paca_trainable=True)
-controlnet = load_controlnet(save_path=unet_save_path, trainable=True)
-dr_module = load_degradation_removal(trainable=True)
-unet = load_unet_control_paca(unet_save_path=unet_save_path, paca_trainable=True)
+# IF YOU HAVE TO TRAIN FROM "SCRATCH", THEN REMEMBER TO ADJUST THE LOAD PATHS ACCORDINGLY
+# in particular, controlnet uses unet, unet has no paca lyaers and dr module is also blank
+vae = load_vae(save_path=load_vae_path, trainable=False)
+unet = load_unet_control_paca(unet_save_path=load_unet_path, paca_save_path=load_paca_layers_path, paca_trainable=True)
+controlnet = load_controlnet(save_path=load_controlnet_path, trainable=True)
+dr_module = load_degradation_removal(save_path=load_degradation_removal_path, trainable=True)
+
 train_loader, val_loader, test_loader = get_dataloaders(manifest_path, batch_size=batch_size, num_workers=num_workers, dataset_class=PairedCTCBCTDatasetNPY, train_size=train_size, val_size=val_size, test_size=test_size)
 train_dr_control_paca(
     vae=vae, 

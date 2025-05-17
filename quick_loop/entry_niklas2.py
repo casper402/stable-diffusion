a3a82529fe7f160a8b59aed83f3a09047f1c4c6e
Niklas Aavad <niklasaavad@gmail.com>
2025-05-17 18:15:08 +0200

    v8

--- quick_loop/entry_niklas2.py
+++ quick_loop/entry_niklas2.py
@@ -32,7 +32,8 @@ warmup_epochs = 5
 # Load pretrained model paths
 # load_dir = "controlnet_v3"
 # load_dir = "controlnet_from_unet_trained_after_joint_round2"
-load_dir = "controlnet_v6"
+# load_dir = "controlnet_v6"
+load_dir = "controlnet_v8-data-augmentation"
 load_vae_path = os.path.join(load_dir, "vae_joint_vae.pth")
 load_unet_path = os.path.join(load_dir, "unet_joint_unet.pth")
 load_controlnet_path = os.path.join(load_dir, "controlnet.pth")
@@ -47,7 +48,8 @@ load_degradation_removal_path = os.path.join(load_dir, "dr_module.pth")
 # save_dir = "controlnet_from_unet_trained_after_joint"
 # save_dir = "controlnet_v4"
 # save_dir = "controlnet_from_unet_trained_after_joint_round2"
-save_dir = "controlnet_v7-data-augmentation"
+# save_dir = "controlnet_v7-data-augmentation"
+save_dir = "controlnet_v8-data-augmentation"
 
 os.makedirs(save_dir, exist_ok=True)
 vae_predict_dir = os.path.join(save_dir, "vae_predictions")
@@ -69,8 +71,8 @@ manifest_path = "../training_data/manifest-filtered.csv"
 # Augmentation
 augmentation = {
     'degrees': (-1, 1),
-    'translate': (0.05, 0.05),
-    'scale': (0.95, 1.05),
+    'translate': (0.1, 0.1),
+    'scale': (0.90, 1.10),
     'shear': None,
 }
 
